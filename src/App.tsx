511aad7ef9be559a7c73144788387422f5dd2ea6
angelomiko129 <angelomikobotabara021@gmail.com>
2025-03-14 10:21:53 +0800

    add scrollyprogress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { motion, useScroll } from "motion/react";
 import Nav from "@parts/Nav.tsx";
 import Socials from "@parts/Socials.tsx";
 import Footer from "@parts/Footer.tsx";
@@ -10,6 +11,8 @@ function App() {
   const [value, setValue] = useState<number>(0);
   const [startAnimation, setStartAnimation] = useState<boolean>(false);
   const [_showPercent] = useState<boolean>(true);
+
+  const { scrollYProgress } = useScroll();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -42,6 +45,19 @@ function App() {
   return (
     // Loading screen
     <div className="text-cWhite relative px-4 sm:mx-20">
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+        }}
+        className="bg-cPink"
+      />
       {!startAnimation && (
         <div className="bg-dark fixed inset-0 z-50 flex h-svh items-center justify-center">
           <NumberTicker

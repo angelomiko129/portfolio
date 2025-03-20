14f60824071e521a4dcccc7f04ddfa5f8d76d3b2
angelomiko129 <angelomikobotabara021@gmail.com>
2025-03-20 14:42:06 +0800

    feat: add TypeScript interfaces for navigation and project components

---
+++ src/constants/props.ts
@@ -0,0 +1,27 @@
+export interface NavProps {
+  startAnimation: boolean;
+  homeRef: React.RefObject<HTMLDivElement>;
+  aboutRef: React.RefObject<HTMLDivElement>;
+  worksRef: React.RefObject<HTMLDivElement>;
+}
+
+export interface TagProps {
+  label: string;
+}
+
+export interface TagsProps {
+  techStack: string[];
+}
+
+export interface TypingTextProps {
+  text: string;
+  speed?: number;
+}
+
+export interface Project {
+  image: string;
+  title: string;
+  githubLink: string;
+  liveLink: string;
+  techStack: string[];
+}
